d42fa896e6e0ababb4410753f5abaad3827e1e09
Nada002 <nada_aloyaidi@outlook.com>
2021-09-03 01:58:15 +0300

    commit 1

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+//adding books
 let books = [[1, "Start with why", "Simon Sinek", 80.0, 13],
              [2, "But how do it know", "J. Clark Scott", 59.9, 22],
              [3, "Clean Code", "Robert Cecil Martin", 50.0, 5],
              [4, "Zero to One", "Peter Thiel", 45.0, 12],
              [5, "You don't know JS", "Kyle Simpson", 39.9, 9]
              ];
+
+             //add a new book
+             books[5] = [6, "Refactoring", "Martin Fowler", 66.5, 30];
+             console.log(books[5]);
+
+             //edit a book
+            books[1][4] = 19;
+            console.log(books[1]);
+
+            //delete a book
+            books.shift();
+
+            //display books
+            console.log(books);
+
