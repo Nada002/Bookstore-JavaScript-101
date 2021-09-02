1198c1042f1b2362ff47a2f5d90d594b8b0b543b
Nada002 <nada_aloyaidi@outlook.com>
2021-09-03 01:13:49 +0300

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+let books = [[1, "Start with why", "Simon Sinek", 80.0, 13],
+             [2, "But how do it know", "J. Clark Scott", 59.9, 22],
+             [3, "Clean Code", "Robert Cecil Martin", 50.0, 5],
+             [4, "Zero to One", "Peter Thiel", 45.0, 12],
+             [5, "You don't know JS", "Kyle Simpson", 39.9, 9]
+             ];
